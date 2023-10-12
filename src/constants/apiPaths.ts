06c1a6e2081ab82b43ed39f30b9cc4e971a5b9c3
Artur <arthurlavrin@gmail.com>
2023-10-12 16:18:08 +0300

    fix paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,4 +1,8 @@
 const API_PATHS = {
+  products:
+    "https://bceuqnc4ec.execute-api.eu-west-1.amazonaws.com/dev/products",
+  getProduct: (id?: string) =>
+    `https://bceuqnc4ec.execute-api.eu-west-1.amazonaws.com/dev/products/${id}`,
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
